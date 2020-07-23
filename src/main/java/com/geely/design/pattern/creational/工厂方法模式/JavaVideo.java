510b7738318059b59b14b6d3f9832f3c0714f4b6
laijunlin <1294074374@qq.com>
2020-07-23 19:08:02 +0800

    1.补充工厂方法

---
+++ src/main/java/com/geely/design/pattern/creational/工厂方法模式/JavaVideo.java
@@ -0,0 +1,9 @@
+package com.geely.design.pattern.creational.工厂方法模式;
+
+public class JavaVideo extends Video {
+
+    @Override
+    public void product() {
+        System.out.println("录制java课程视频");
+    }
+}
